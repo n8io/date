08f13a64abe4f2bf48c02388bfbcc4f1adc90c36
Nate Clark <n8io@users.noreply.github.com>
2024-01-11 16:45:18 -0500

    fix: 🐛 Validate the plain date string format

--- src/plainDateStringToTimeZoneDate/index.test.ts
+++ src/plainDateStringToTimeZoneDate/index.test.ts
@@ -122,4 +122,48 @@ describe('plainDateStringToTimeZoneDate', () => {
       })
     }
   })
+
+  describe('when the plain date string is empty', () => {
+    test('should throw an error', () => {
+      const formatter = makeTimeZoneOffsetFormatter(timeZones.DENVER)
+
+      expect(() => plainDateStringToTimeZoneDate('', formatter)).toThrowError('Plain date string is required')
+    })
+  })
+
+  describe('when the plain date string format is valid', () => {
+    const formatter = makeTimeZoneOffsetFormatter(timeZones.DENVER)
+
+    const cases = [
+      { input: '2023-03-23T00:19', desc: 'with a T' },
+      { input: '2023-03-23 21:19', desc: 'with a space' },
+      { input: '2023-02-23', desc: 'without a time' },
+      { input: '2023-02', desc: 'without a day' },
+      { input: '2023', desc: 'without a month' },
+      { input: '2023-05-23T09:19:34.324', desc: 'with milliseconds and a T' },
+      { input: '2023-12-23 13:19:34.324', desc: 'with milliseconds and a space' },
+    ]
+
+    test.each(cases)('should not throw an error for $input', ({ input, desc }) => {
+      expect(() => plainDateStringToTimeZoneDate(input, formatter)).not.toThrowError()
+    })
+  })
+
+  describe('when the plain date string format is NOT valid', () => {
+    const formatter = makeTimeZoneOffsetFormatter(timeZones.DENVER)
+
+    const cases = [
+      { input: '2023-03-23T00:19Z', desc: 'with a Z' },
+      { input: '2023-03-23 21:19 GMT', desc: 'with a time zone' },
+      { input: '2023-03-23T21:19+01:30', desc: 'with a time zone offset' },
+      { input: 'ABC', desc: 'Completely wrong' },
+      { input: null, desc: 'null' },
+      { input: undefined, desc: 'given undefined' },
+      { input: 12_345, desc: 'with a number' },
+    ]
+
+    test.each(cases)('should throw an error $desc $input', ({ input }) => {
+      expect(() => plainDateStringToTimeZoneDate(input as string, formatter)).toThrowError()
+    })
+  })
 })
--- src/plainDateStringToTimeZoneDate/index.ts
+++ src/plainDateStringToTimeZoneDate/index.ts
@@ -6,11 +6,26 @@ import {
 } from '../getTimeZoneUtcOffsetInMinutes'
 
 const systemFormatter = makeTimeZoneOffsetFormatter(Intl.DateTimeFormat().resolvedOptions().timeZone)
+const formatRegex = /^\d{4}(?:-\d{2}(?:-\d{2}(?:(?:T|\s)\d{2}:\d{2}(?::\d{2}(?:\.\d{1,3})?)?)?)?)?$/
+
+const checkPlainDateString = (plainDateString: string): void => {
+  if (!plainDateString) {
+    throw new Error('Plain date string is required')
+  }
+
+  if (formatRegex.test(plainDateString) === false) {
+    throw new Error(
+      'Plain date string must not contain an offset or end in "Z". Valid formats include "YYYY-MM-DD" and "YYYY-MM-DD HH:mm:ss"'
+    )
+  }
+}
 
 const plainDateStringToTimeZoneDate = (
   plainDateString: string,
   timeZoneOffsetFormatter: TimeZoneOffsetFormatter
 ): Date => {
+  checkPlainDateString(plainDateString)
+
   const date = new Date(plainDateString)
   const systemTimeZoneOffsetMilliseconds = getTimeZoneUtcOffsetInMinutes(date, systemFormatter) * millisecondsInAMinute
   const requestedTimeZoneOffsetInMinutes = getTimeZoneUtcOffsetInMinutes(date, timeZoneOffsetFormatter)
